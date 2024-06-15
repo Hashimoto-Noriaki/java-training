61dcfc2ce6aca7dd4b600dc56d84a0ce760bcb9a
nori.hasimo.89@gmail.com <nori.hasimo.89@gmail.com>
2024-06-15 13:18:24 +0900

    first commit

---
+++ greeting.java
@@ -0,0 +1,15 @@
+//クラス
+class Greeting{
+    //メソッド
+    public static void main(String args[]){
+        //処理の部分
+        System.out.println("おはようございます");
+        System.out.println("こんにちは");
+        System.out.println("こんばんは");
+        //処理の終わりにセミコロンつける
+    }
+}
+
+// おはようございます
+// こんにちは
+// こんばんは
