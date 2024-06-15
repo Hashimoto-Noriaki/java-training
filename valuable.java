164348856e4d78a91000c410e2bd0b430bdbe25b
nori.hasimo.89@gmail.com <nori.hasimo.89@gmail.com>
2024-06-15 16:44:37 +0900

    変数の練習

---
+++ valuable.java
@@ -0,0 +1,15 @@
+class Valuable_lesson {
+    public static void main(String[] args){
+        int var;//変数の宣言
+        var = 1;//変数の代入
+
+        System.out.println(var);
+    }
+}
+//1
+
+//変数のルール
+//予約語は変数にできない　return while for class
+//アルファベット　数字　　アンダースコア　　$
+//大文字と小文字は区別される
+
